9d5b191fb26008519125f0c577bc78d7feccb57f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-09 04:15:55 -0400

    fix crop and tonemapcrop as well as add merge

--- crop.sh
+++ crop.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "$1" -c:v libx264 -map 0:v? -vf "$3" -b:v "$4" -map 0:a? -c:a copy -map 0:s? -c:s copy "$2"
+ffmpeg -i "$1" -c:v libx264 -map 0:v? -vf "$3" -b:v "$4" -map 0:a? -c:a copy -map 0:s? -c:s copy -max_muxing_queue_size 2048 "$2"
--- tonemapcrop.sh
+++ tonemapcrop.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "$1" -c:v libx265 -map 0:v? -vf $3,zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=1,zscale=t=bt709:m=bt709:r=tv,format=yuv420p -b:v $4 -map 0:a? -c:a copy -map 0:s? -c:s copy -max_muxing_queue_size 1024 "$2"
+ffmpeg -i "$1" -c:v libx265 -map 0:v? -vf $3,zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=1,zscale=t=bt709:m=bt709:r=tv,format=yuv420p -b:v $4 -map 0:a? -c:a copy -map 0:s? -c:s copy -max_muxing_queue_size 2048 "$2"
